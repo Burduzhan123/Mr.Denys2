ebc79a423a681a90ac33e5b598564515753d1b70
Denys <denisburduzan@gmail.com>
2023-12-02 19:13:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var num = 'number' + 23 + 32
+console.log(num)
+var num = 41 + 1 + 'number'
+console.log(num)
+var num = null + 1
+console.log(num)
+var num = 'five' + + 'two'
+console.log(num)
+var num = 2 && 7
+console.log(num)
+var num = +'40' + +'2'
+console.log(num)
+var num = true + false
+console.log(num)
+var num = '4px' - 3
+console.log(num)
+console.log('4pg' - 3)
+console.log('4' - 3)
+console.log(12 / '6')
+console.log(23 + 42 + 'number')
+console.log('10' - 5 === 6)
+console.log('10' + (5 === 6))
+console.log('2' + 3 ** 2)
+console.log(undefined + 1)
+console.log('true' == true)
+console.log(false == 'false')
+console.log(null == '')
+console.log(3 ** (9 / 3))
+console.log(1 < 2 < 3)
+console.log(3 ** 2 / 3)
+console.log(1 < 2 > 3)
+console.log((+null == false) < 1)
+var condition = false && true || true
+var condition = false && (true || true)
+console.log(+null == false) < 1 ** 5
+console.log('foo'+ + +'bar')
+console.log(+null)
+console.log(null == '')
+console.log(0 || '0' && 1)
+console.log('0' && 7)
+console.log(0 || 7)
+console.log('1' == 1)
+console.log((+null == false) < 1)
+console.log(!!'false' == !!'true')
